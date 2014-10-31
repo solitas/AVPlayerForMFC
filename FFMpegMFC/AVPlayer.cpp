7f20be33a9ec913f7809a8e38bd90b9b4cb682ad
����� <solitasroh@gmail.com>
2014-10-31 21:44:06 +0900

    클래스 초기화
    AVPlayer 초기화 중...

--- FFMpegMFC/AVPlayer.cpp
+++ FFMpegMFC/AVPlayer.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "AVPlayer.h"
+#include "AudioDec.h"
+#include "VideoDec.h"
 
 
 AVPlayer::AVPlayer()
@@ -13,5 +15,32 @@ AVPlayer::~AVPlayer()
 
 bool AVPlayer::play_video(const char* resourceName)
 {
+	av_register_all();
+
+	AVFormatContext* pFormatCtx = NULL;
+
+	if (avformat_open_input(&pFormatCtx, resourceName, NULL, NULL) < 0)
+	{
+		TRACE("format context open error");
+		return false;
+	}
+
+	if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
+	{
+		TRACE("find stream info error");
+		return false;
+	}
+
+	for (int i = 0; i < pFormatCtx->nb_streams; i++)
+	{
+		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO)
+		{
+
+		}
+		if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+
+		}
+	}
 	return true;
 }
